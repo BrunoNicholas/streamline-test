fd001e559d3416cb6a1f53d2e8d5469ad5e08fcc
BrunoNicholas <sbnibro256@gmail.com>
2023-03-28 16:35:51 +0300

    adding more features

--- database/migrations/2023_03_28_131110_create_medical_reports_table.php
+++ database/migrations/2023_03_28_131110_create_medical_reports_table.php
@@ -13,9 +13,16 @@ return new class extends Migration
     {
         Schema::create('medical_reports', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('doctor_id')->nullable();
+            $table->unsignedBigInteger('nurse_id')->nullable();
 
+            $table->text('recorded_symptoms')->nullable();
+            $table->string('lab_tests')->nullable();
             $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('doctor_id')->references('id')->on('villages')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('nurse_id')->references('id')->on('nurses')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            RegionsTableSeeder::class,
+            DistrictsTableSeeder::class,
+            LabTestsTableSeeder::class,
+            VillagesTableSeeder::class,
+        ]);
     }
 }
